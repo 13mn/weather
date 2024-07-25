4850456311b7fad81799598d6345639d25061496
Zhao Siqi <89009573+13mn@users.noreply.github.com>
2024-07-26 01:35:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask,render_template,request
 app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
-    return(render_template("test01.html"))
+    return(render_template("weather.html"))
 if __name__ == "__main__":
     app.run()
